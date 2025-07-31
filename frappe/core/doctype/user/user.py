0cbdc67094c424ee85bf3a259a516e1f000311fb
IsHimanshu <51610605+IsHimanshu@users.noreply.github.com>
2025-07-31 21:44:45 +0900

    push

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -320,7 +320,7 @@ class User(Document):
 				self.set(field, sanitize_html(field_value, always_sanitize=True))
 
 	def set_full_name(self):
-		self.full_name = " ".join(filter(None, [self.first_name, self.last_name]))
+		self.full_name = " ".join(filter(None, [self.last_name, self.first_name]))
 
 	def check_enable_disable(self):
 		# do not allow disabling administrator/guest
